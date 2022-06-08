368b19fbf4aa4465c723a58cfde23a9f30485115
Dave Toerber <david.toerber@charter.com>
2022-06-08 09:24:35 -0600

    add speed is good for

--- src/app/store-front/speed-is-good-for/speed-is-good-for.component.ts
+++ src/app/store-front/speed-is-good-for/speed-is-good-for.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-speed-is-good-for',
   templateUrl: './speed-is-good-for.component.html',
-  styleUrls: ['./speed-is-good-for.component.scss']
+  styleUrls: ['./speed-is-good-for.component.scss'],
 })
 export class SpeedIsGoodForComponent implements OnInit {
+  /**
+   * Heading for Speed Is Good For section
+   */
+  @Input() speedIsGoodForBulletsHeadline: string = 'Speed Is Good For';
+  @Input() listItems: string[] = [
+    'HD streaming on multiple devices',
+    'Online gaming',
+    'Downloading shows, movies and large files',
+    'Supporting multiple connected smart home devices',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
